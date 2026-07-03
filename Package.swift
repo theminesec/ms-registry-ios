4f099ae44c2ee34e0a441d8bc8d95efa589d632d
git-action <gitflow@minesecsoftpos.com>
2026-07-03 05:22:28 +0000

    Release Headless iOS SDK v1.2.1

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Headless",
-            url: "https://github.com/theminesec/ms-registry-ios/releases/download/v1.2.1/Headless.xcframework.zip",
-            checksum: "629c3dc6ecc361a67dded891592d74dcd3f75e56d63390e5847063675fa6620c"
+            url: "https://github.com/theminesec/ms-registry-ios/releases/download/1.2.1/Headless.xcframework.zip",
+            checksum: "2a3a1aa6c4cc6e9824a2d2aa63cb2215477fc57508a44a92bad0a3028ae59246"
         )
     ]
 )
